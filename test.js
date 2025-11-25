9f087d1db61f8d6579b92c68d023efeb3003f801
chan-acn <thiruloga.chandru@accenture.com>
2025-11-25 06:42:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// Correct camelCase
+// Correct camelCas
 let userName = "Alice";
 const totalCount = 10;
 
@@ -10,6 +10,7 @@ let UserName = "Charlie";
 
 // Incorrect (all caps)
 const TOTAL_COUNT = 20;
+
 
 
 
