b5f522a894756d5c5314447a49f60ea9546d4a8b
chan-acn <thiruloga.chandru@accenture.com>
2025-12-03 10:17:09 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-// Badly named function
-function do_stuff() {
-    console.log("Doing stuff");
-}
 
-// Another non-camelCase function
-function process_data() {
-    return "processed";
-}
-
-// Good camelCase function
-function calculateTotal(price, tax) {
-    return price + tax;
-}
