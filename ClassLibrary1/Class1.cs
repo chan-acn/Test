af471f6f69a480033960c66bbe8080539af6a3f3
chan-acn <thiruloga.chandru@accenture.com>
2026-01-20 21:24:13 +0530

    test

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xrm.Sdk.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        ExecuteMultipleRequest test = new ExecuteMultipleRequest();
+    }
+}
